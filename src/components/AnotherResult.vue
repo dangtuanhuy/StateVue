21463dbdc6ac2c506e505060b2a2281ee04c945f
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-14 17:13:30 +0700

    source

---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<p>Counter is: {{doubleCounter}}</p>
+		<hr>
+		<p>Number of click: {{stringCounter}}</p>
+	</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+	computed:{
+		// counter() {
+		// 	return this.$store.getters.doubleCounter;
+		// },
+		// clicks() {
+		// 	return this.$store.getters.stringCounter;
+		// }
+        ...mapGetters(["doubleCounter", "stringCounter"]),
+        
+
+    }
+};
+</script>
+
+<style>
+</style>
